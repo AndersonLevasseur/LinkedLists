42d6bf93443d56a15c488c26a5759734e33e7921
Anderson Levasseur <anderson.levasseur@gmail.com>
2021-01-24 13:02:36 -0500

    Overhauled - now it works

--- permutationsOfString.js
+++ permutationsOfString.js
@@ -1,38 +1,25 @@
-let i = 0
-const array = ["a", "b"];
-let newPermutation;
-
-function permutate(array) {
-    let original = array
-    let left = array[i];
-    let right = array[array.length - 1];
-    if (left === right) {
-        return `${array.toString()}; ${newPermutation}`;
-    } else {
-        i++
-        let permutation = array;
-        let delObj = permutation[1];
-        do {
-            permutation.splice(1, 1);
-            permutation.push(delObj);
-        } while (array !== permutation) {
-            oldPermutation = permutation.toString();
-            delObj = permutation[1];
-            permutation.splice(1, 1);
-            permutation.push(delObj);
-            newPermutation = `${oldPermutation}; ${newPermutation}`;         
-        } 
-        delObj = permutation[0];
-        permutation.splice(0, 1);
-        permutation.push(delObj);
-
-        // Before move to next cycle to the next letter 
-        // abc, bac
+const str = "abcdefg"
+function permutate(str) {
+    if (str.trim().length === 0 || typeof (str) !== "string") {
+        throw "invalid input";
     }
-    return newPermutation, permutate(array)
-    // continue until left reaches right 
-    // https://media.geeksforgeeks.org/wp-content/cdn-uploads/NewPermutation.gif
-    // return multiple strings
+    if (str.length === 1) {
+        return [str];
+    }
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+        const freeze = str[i];
+        const firstUnfrozen = str.substring(0, i);
+        const lastUnfrozen = str.substring(i + 1, str.length);
+        const unfrozen = firstUnfrozen + lastUnfrozen;
+        const permutations = permutate(unfrozen);
+      
+        for (const permutation of permutations) {
+            result.push(freeze.concat(permutation));
+        }
+    }
+    return result;
+
 }
-console.log(permutate(array));
-console.log(typeof(permutate(array)));
+console.log(permutate(str)); 
+
